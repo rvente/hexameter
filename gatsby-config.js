a9cd00409c55f225723139b0051a8290bdc415c3
Ralph Vente <venteralph@gmail.com>
2020-06-11 00:48:47 -0400

    add styling to image on about page

--- gatsby-config.js
+++ gatsby-config.js
@@ -60,9 +60,9 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `minimal-blog - @lekoarts/gatsby-theme-minimal-blog`,
-        short_name: `minimal-blog`,
-        description: `Typography driven, feature-rich blogging theme with minimal aesthetics. Includes tags/categories support and extensive features for code blocks such as live preview, line numbers, and code highlighting.`,
+        name: `Dactylic Hexameter`,
+        short_name: `hexameter`,
+        description: `Blake is a CS major exploring the worlds of AI, Computer Vision, Linguistics, and Generative Art`,
         start_url: `/`,
         background_color: `#fff`,
         theme_color: `#6B46C1`,
