a5a67e7611433505f8442219afee27aed4cf5435
Ralph Vente <venteralph@gmail.com>
2020-07-08 23:55:36 -0400

    add arxiv links to respective math fragments

--- content/posts/2020-06-09--Math/math_game.js
+++ content/posts/2020-06-09--Math/math_game.js
@@ -1,3 +1,22 @@
+/*
+ *  Copyright Ralph "Blake" Vente, 2020
+
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ * 
+ *  Warning: HIC SVNT DRACONES. This code employs many questionable practices.
+ * 
+ */
 import React, {useState} from 'react'; 
 
 import 'katex/dist/katex.min.css';
@@ -7,8 +26,8 @@ import HumanFormulas from './Fixtures/human_out.json';
 import AIFormulas from './Fixtures/ai_out.json';
 
 // regard these colors as enum types for component state
-// a useful shortcut -- maintaining one state var instead
-// of many
+// a useful shortcut 
+
 const correct = 'rgba(151, 226, 153, .9)';
 const hint = 'rgba(151, 226, 153, .4)';
 const unselected = 'rgba(255, 255, 255, .1)';
@@ -16,22 +35,25 @@ const fullWhite = 'white'
 
 // why is this global? good question: I wanted to keep things concise, so
 // I drew out a fragile state machine and thus minimized code lines, sacrificing
-// good style along the way
-let human;
-let ai;
-let duo;
+// good style along the way. As I added functionality, things got worse quick.
+let human = {creator: "Human", expression: "\\text{A human wrote me. Pick me!}"};
+let ai = {creator: "AI", expression: "\\text{An RNN wrote this. Can you beat me?}"};
+let duo = [human, ai];
+let h_label = "<Source Paper>";
 
 // hack: prevent double clicks from shifting state
 // color is reset upon press, so double press will fail
 function populateGlobals(selection) {
   if (selection === correct) {
-    human = {creator: 'Human', expression: getRandomElem(HumanFormulas)};
+    const {label, real_exp} = getRandomCorrect(HumanFormulas);
+    human = {creator: 'Human', expression: real_exp};
     ai = {creator: 'AI', expression: getRandomElem(AIFormulas)};
     duo = shuffle([human,ai]);
-    return unselected
+    h_label = label;
+    return unselected;
   }
  }
- populateGlobals(correct);
+//  populateGlobals(correct);
 
 export const GuessWho = () => { 
   const [selection, setColor] = useState(unselected);
@@ -50,6 +72,7 @@ export const GuessWho = () => {
       alignItems: 'center',
       justifyContent: 'center',
       borderRadius: '10px',
+      overflow: 'hidden',
       padding: '20px',
       height: '75px',
       border: 'rgba(0,0,0,.1) thin solid',
@@ -91,8 +114,17 @@ export const GuessWho = () => {
  }
  function setWinMsg(selection) {
    if (selection === correct) {
-  return getRandomElem(['Good job! Another?', 'Nice work! Have another go?', 'Remarkable detective work. Go again?', "Winner, winner! One more time?"])
-   }
+     const win_msg = ['Good job! Another?', 'Nice work! Have another go?', 'Remarkable detective work. Another mission?', "Winner, winner! One more time?"]
+  return (
+    <div>
+      <a
+      href={`https://arxiv.org/abs/${h_label}`}
+      style={{color: 'black'}}
+      >{ `From ${h_label}`}</a> &nbsp;
+      {getRandomElem(win_msg)}
+    </div>
+    )
+  }
  }
 
 function revealTrue(creator) {
@@ -108,6 +140,9 @@ function rendering(el) {
   try {
     katex.renderToString(el, {
       displayMode: false,
+      throwOnError:true,
+      strict: "error",
+      trust: false,
       macros: {
         "\\RR": "\\mathbb{R}",
         "\\textup": "\\text",
@@ -120,12 +155,31 @@ function rendering(el) {
 }
 
 function getRandomElem(indexable) {
-  const expression = indexable[getRandomInt(indexable.length)];
-  return rendering(expression) ? expression :  getRandomElem(indexable);
+  let expression = indexable[getRandomInt(indexable.length)];
+  while (! rendering(expression) ) {
+    expression = indexable[getRandomInt(indexable.length)].substring(0,400);
+  }
+  return expression;
 }
+
+function getRandomCorrect(indexable) {
+  const labelLen = "1801.00497".length;
+  let exp= indexable[getRandomInt(indexable.length)].replace(/%(.*?)/g, "").substring(0,200);
+  while (! rendering(exp) ) {
+    exp= indexable[getRandomInt(indexable.length)];
+  }
+  console.log(exp)
+  return {
+    label: exp.substring(0,labelLen),
+    real_exp: exp.substring(labelLen)
+  }
+
+}
+
 
 function shuffle(array) {
   array.sort(() => Math.random() - 0.5);
   return array
 }
 
+ 
