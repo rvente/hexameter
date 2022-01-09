ddf0853dbbf337a5240fc5202aec098b1ecc26a4
Blake Vente <venteralph@gmail.com>
2022-01-09 01:36:28 -0500

    Md2ssml, twitter, env vars

--- content/posts/2022-01-08--Md2ssml/convert.js
+++ content/posts/2022-01-08--Md2ssml/convert.js
@@ -1,52 +1,81 @@
-import React, { useState, useEffect } from "react";
-
+import React, { useState, useEffect, useCallback } from "react";
+import debounce from 'lodash.debounce';
 
 /*
  * @author Blake Vente
- * @reference https://p5js.org/examples/simulate-particle-system.html
- */
-
-/*
- * @source https://stackoverflow.com/a/47317538
  */
 
 
+const ENDPOINT = process.env.GATSBY_PANDOC_ENDPOINT;
+const MARKUP = "# edit me! \n## numbered list example \n1. first item\n2. second item";
+
+
+const darkBackground = {
+  color: "rgb(214, 222, 235)",
+  backgroundColor: "rgb(1, 22, 39)",
+};
+const inputField = {
+  width: "100%",
+  height: "400px",
+  border: "dotted",
+  padding: "-10px",
+  fontSize: "1rem",
+  borderRadius: "10px",
+  color: "var(--theme-ui-colors-text)",
+  backgroundColor: "var(--theme-ui-colors-transparent,transparent)",
+}
+const breakStyle = {
+  margin: '20px',
+};
 
 export default function Convert() {
   var myHeaders = new Headers();
   myHeaders.append("Content-Type", "application/json");
 
-  var raw = JSON.stringify({
-    "markup": "# hello world\n## numbered list example \n1. first item\n2. second item"
-  });
+  const [convertReponse, setConvertResponse] = useState("");
+  const [markupRequest, setMarkupRequest] = useState(MARKUP);
 
-  var requestOptions = {
-    method: 'POST',
-    headers: myHeaders,
-    body: raw,
-    redirect: 'follow'
+  const HitConvertEndpoint = async (r) => {
+    const requestOptions = {
+      method: 'POST',
+      headers: myHeaders,
+      body: JSON.stringify({ "markup": r }),
+      redirect: 'follow'
+    };
+    const res = await fetch(ENDPOINT, requestOptions)
+      .then(response => response.text())
+      .catch(error => console.log('error', error));
+
+    setConvertResponse(JSON.parse(res)?.result);
   };
 
-  const [convertReponse, setConvertResponse] = useState("");
+  const handleInputChange = (e) => {
+    // on input change debounce query api
+    const requestString = e.target.value;
+    setMarkupRequest(requestString);
+    debouncedQuery(requestString);
+  };
 
   useEffect(() => {
-    const HitConvertEndpoint = async (r) => {
-      const res = await fetch("", requestOptions)
-        .then(response => response.text())
-        .catch(error => console.log('error', error));
-
-      setConvertResponse(JSON.parse(res)?.result);
-      console.log(process.env.ENABLE_GATSBY_REFRESH_ENDPOINT);
-      console.log(res);
-      console.log(JSON.parse(res));
-    };
-    HitConvertEndpoint("");
+    // initially set to default string
+    HitConvertEndpoint(MARKUP);
   }, []);
 
-  const dropShadow = { color: "rgb(214, 222, 235)", backgroundColor: "rgb(1, 22, 39)"};
+  const debouncedQuery = useCallback(debounce(m =>
+    // don't repeat api request until this many ms of pause time
+    HitConvertEndpoint(m), 500), []
+  )
   return (
-    <div style={dropShadow} className="gatsby-highlight">
-      <pre className="prism-code language-xml">{convertReponse}</pre>
-    </div>
+    <section>
+      <textarea 
+        value={markupRequest}
+        onChange={handleInputChange}
+        style={inputField}>
+      </textarea>
+      <div style={breakStyle}></div>
+      <div style={darkBackground} className="gatsby-highlight">
+        <pre className="prism-code language-xml">{convertReponse}</pre>
+      </div>
+    </section>
   )
 }
